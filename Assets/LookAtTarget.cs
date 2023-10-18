3c7e023b92c4859c60f38c7c7e0a6ede490b9075
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2023-10-18 14:50:05 +0200

    Added arrow

---
+++ Assets/LookAtTarget.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        transform.LookAt(target.transform);
+    }
+}
