e7389dd8ea35309a774b7b46d8fe2953803141bc
Shiro <kodokododk@gmail.com>
2023-10-19 14:03:54 +0200

    Firefly stuff, and Device simulator fix

--- Assets/Scripts/Firefly behaviour.cs
+++ Assets/Scripts/Firefly behaviour.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireflybehaviour : MonoBehaviour
+public class Fireflybehaviour : MonoBehaviour, Interface_AttentionMethod
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 1f;
+    Vector3 startPosition;
+    Vector3 endPosition;
+
+    private void Start()
     {
-        
+        startPosition = gameObject.transform.position;
+        endPosition = gameObject.transform.position;
+    }
+
+    // Start is called before the first frame update
+    public void DoMethod (GameObject target)
+    {
+        startPosition = gameObject.transform.position;
+        endPosition = target.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(startPosition, endPosition, Time.deltaTime * speed);
     }
 }
