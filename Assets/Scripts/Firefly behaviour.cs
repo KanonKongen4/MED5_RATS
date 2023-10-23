8c982bd52a9d963e21e51550e5a2cd75df845e41
Shiro <kodokododk@gmail.com>
2023-10-23 10:36:39 +0200

    Omg I fixed the firefly woahahaa

--- Assets/Scripts/Firefly behaviour.cs
+++ Assets/Scripts/Firefly behaviour.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class Fireflybehaviour : MonoBehaviour, Interface_AttentionMethod
 {
+    public GameObject fireflyObj;
     public float speed = 1f;
+    
     Vector3 startPosition;
     Vector3 endPosition;
 
     private void Start()
     {
-        startPosition = gameObject.transform.position;
-        endPosition = gameObject.transform.position;
+        startPosition = fireflyObj.transform.position;
+        endPosition = fireflyObj.transform.position;
     }
 
     // Start is called before the first frame update
     public void DoMethod (GameObject target)
     {
-        startPosition = gameObject.transform.position;
-        endPosition = target.transform.position;
+        startPosition = fireflyObj.transform.position;
+        if (target.GetComponent<Renderer>() != null)
+        {
+            endPosition = target.GetComponent<Renderer>().bounds.center;
+        }
+        else
+        {
+            endPosition = target.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(startPosition, endPosition, Time.deltaTime * speed);
+        fireflyObj.transform.position = Vector3.Lerp(fireflyObj.transform.position, endPosition, Time.deltaTime * speed);
     }
 }
