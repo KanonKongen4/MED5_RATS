feaf6ec80421ead3f12ace5432722ddb4cdf1dce
Shiro <kodokododk@gmail.com>
2023-10-25 11:52:51 +0200

    Implemented scene changer
    Can be enabled/disabled in Test 0 by enabling/disabling the scene changer object.

---
+++ Assets/)Our stuff/Scripts/TestScenes Randomizer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Properties;
+using UnityEngine;
+
+public class TestScenesRandomizer : MonoBehaviour
+{
+    private List<int> sceneList = new List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        RandomizeScene();
+    }
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        RandomizeScene();
+    }
+
+    void RandomizeScene ()
+    {
+        DoorScenechange newDoor = FindObjectOfType<DoorScenechange>();
+        int nextDoor = Random.Range(0, sceneList.Count);
+        newDoor.changeTo = sceneList[nextDoor];
+        sceneList.RemoveAt(nextDoor);
+    }
+}
