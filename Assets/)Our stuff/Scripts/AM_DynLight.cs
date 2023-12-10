f39a4c602d35bb420595c592e7b90d387f90a46a
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2023-12-10 22:47:41 +0100

    Added adm script

--- Assets/)Our stuff/Scripts/AM_DynLight.cs
+++ Assets/)Our stuff/Scripts/AM_DynLight.cs
@@ -7,7 +7,7 @@ public class AM_DynLight : MonoBehaviour, Interface_AttentionMethod
     public GameObject target;
     public Transform lightTransform;
     private Vector3 lookVector;
-    public bool useSmoothRotation;
+
     public float rotationSpeed = 1f;
     public void DoMethod(GameObject target)
     {
@@ -18,13 +18,7 @@ public class AM_DynLight : MonoBehaviour, Interface_AttentionMethod
     {
         if (target == null) return; //So below is not run if target is null..
 
-
-        if (!useSmoothRotation)
-            lightTransform.LookAt(target.GetComponent<Renderer>().bounds.center);
-        else
-        {
             lookVector = ( target.GetComponent<Renderer>().bounds.center-lightTransform.position).normalized; // find direction & normalize vector so max length is 1. So just direction
             lightTransform.rotation = Quaternion.Lerp(Quaternion.LookRotation(lightTransform.transform.forward), Quaternion.LookRotation(lookVector), Time.deltaTime * rotationSpeed);
-        }
     }
 }
